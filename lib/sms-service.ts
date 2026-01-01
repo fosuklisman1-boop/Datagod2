3bb96d01ae79ea6f86f6f3cda0f389aa830fb307
Developer <developer@datagod2.com>
2026-01-01 12:04:05 +0000

    chore: Change SMS sender ID fallback from DGOD to CLINGSHUB2

--- lib/sms-service.ts
+++ lib/sms-service.ts
@@ -114,7 +114,7 @@ export async function sendSMS(payload: SMSPayload): Promise<SendSMSResponse> {
     console.log('[SMS] Sending to:', normalizedPhone, '- Message:', payload.message.substring(0, 60))
 
     const vasKey = process.env.MOOLRE_API_KEY || ''
-    const senderId = process.env.MOOLRE_SENDER_ID || 'DGOD'
+    const senderId = process.env.MOOLRE_SENDER_ID || 'CLINGSHUB2'
     
     // Build query parameters
     const queryParams = new URLSearchParams({
