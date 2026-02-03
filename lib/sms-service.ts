24436a569870ed5a5df71986a908076d235710cb
Developer <developer@datagod2.com>
2026-02-03 13:01:45 +0000

    update default SMS sender ID to CLINGDTGOD

--- lib/sms-service.ts
+++ lib/sms-service.ts
@@ -122,7 +122,7 @@ export async function sendSMS(payload: SMSPayload): Promise<SendSMSResponse> {
     console.log('[SMS] Sending to:', normalizedPhone, '- Message:', payload.message.substring(0, 60))
 
     const vasKey = process.env.MOOLRE_API_KEY || ''
-    const senderId = process.env.MOOLRE_SENDER_ID || 'BMSG'
+    const senderId = process.env.MOOLRE_SENDER_ID || 'CLINGDTGOD'
 
     // Build query parameters
     const queryParams = new URLSearchParams({
